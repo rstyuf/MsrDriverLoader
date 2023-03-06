728c3b3fbdadf0ad8fce11d95e86f4468695ae6a
rstyuf <120020518+rstyuf@users.noreply.github.com>
2023-03-06 14:32:36 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Ring0DriverLoader.Interop;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Ring0DriverLoader
+{
+ 
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //NativeMethods.AllocConsole();
+            Console.WriteLine("++++ Ring0 Driver Loader ++++");
+            if (args.Length < 1 || args[0] == "keep_open")
+            {
+                Console.WriteLine("### Loading driver ###");
+                Ring0.Open();
+                Console.WriteLine("< Loaded: will keep open until closed by pressing ENTER key >");
+                Console.ReadLine();
+
+                Console.WriteLine("--- Unloading driver ---");
+                Ring0.Close();
+                Console.WriteLine(" Goodbye! ");
+
+                // Console.WriteLine("Need an argument: Either 'load' or 'unload' or 'keep_open'");
+                // Console.WriteLine("Note: 'unload' currently doesn't work yet so 'keep_open' is recommended");
+            }
+            else if (args[0] == "load")
+            {
+                Console.WriteLine("### Loading driver ###");
+                Ring0.Open();
+            }
+            else if (args[0] == "unload")
+            {
+                Console.WriteLine("--- Unloading driver ---");
+                Ring0.Close();
+            }
+            /*else if (args[0] == "keep_open")
+            {
+                Console.WriteLine("### Loading driver ###");
+                Ring0.Open();
+                Console.WriteLine("< Loaded: will keep open until closed by pressing ENTER key >");
+                Console.ReadLine();
+
+                Console.WriteLine("--- Unloading driver ---");
+                Ring0.Close();
+                Console.WriteLine(" Goodbye! ");
+
+            }*/
+            else
+            {
+                Console.WriteLine("Need an argument: Either 'load' or 'unload' or 'keep_open'");
+                Console.WriteLine("Note: 'unload' currently doesn't work yet so 'keep_open' is recommended");
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# MsrDriverLoader
- A simple loader for the WinRing0 Driver
+# Ring0 Driver Loader
+
+Adopted from clamchowder's  MsrUtil tool (Performance Counter Reader) found at github.com/clamchowder/MsrUtil
+
+I admit I'm not great at C# or Windows development in general, so I'm not 100% on having done things the right way but it seems to be functional. This tool is used to load the WinRing0 driver so that I can use it in C code.
+
+# THIS SOFTWARE IS CONSIDERED EXPERIMENTAL. 
+
+## Building
+Open the sln in Visual Studio, hit build.
+
+## Running
+The code needs to be run as admin to function. 
+It takes arguments: 
+> 'load' loads the driver (currently no way to unload though without restarting)
+> 'unload' doesn't work (I need to implement it but I don't have time)
+> 'keep_open' loads the driver and keeps it open until you give it any input (Enter key press) and then unloads and exits. This is the recommended mode.
+Keep_open is the default for no arguments. 
+To run either run from an admin privileges cmd instance or right click, run as admin for keep_open. It needs admin privileges to use the winring0 driver.
